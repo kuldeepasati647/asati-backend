55dec98123bbef17bccf870506d890320b3fe2e3
ramjeemishra <ramjeemishra23@gmail.com>
2025-11-22 15:16:05 +0530

    render port fixed

--- server.js
+++ server.js
@@ -15,4 +15,6 @@ mongoose.connect(process.env.MONGO_URI)
 
 app.use("/api/users", userRoutes);
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
